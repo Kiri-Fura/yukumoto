cb087ce301e46248a895361fe947482c3948cef3
Kiri-Fura <rabirabipc@gmail.com>
2025-12-21 06:50:29 +0900

    Add files via upload

---
+++ kirifuras-video/script.js
@@ -0,0 +1,17 @@
+"use strict";
+function renderVideos() {
+    const grid = document.getElementById('video-grid');
+    if (!grid)
+        return;
+    grid.innerHTML = myVideos.map(video => `
+    <div class="video-card" onclick="location.href='${video.url}'">
+      <img src="${video.thumbnail}" class="video-thumbnail" alt="thumbnail">
+      <div class="video-info">
+        <h3>${video.title}</h3>
+      </div>
+    </div>
+  `).join('');
+}
+window.onload = () => {
+    renderVideos();
+};
